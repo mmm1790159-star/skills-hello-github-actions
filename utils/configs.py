bfe2c7f2984f761f97817cee6d310d85b77f934f
mmm1790159-star <m.m.m1790159@gmail.com>
2026-01-07 17:53:43 +0330

    Add notification modules: SMS and Notification Manager, and entry script for sending messages.

--- utils/configs.py
+++ utils/configs.py
@@ -1,6 +1,1 @@
-# configs.py
-# Stores sensitive configurations such as API keys, etc.
-
-# Future implementation goes here.
-
-# WARNING: Do not share sensitive details in public repositories.
+# utils/configs.py content here
